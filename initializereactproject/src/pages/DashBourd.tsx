877c842396719a4d1d630a6ec7d31d269560a93e
savindaJ <thantrige32@gmail.com>
2024-06-01 12:58:04 +0530

    add card with fix ui

--- initializereactproject/src/pages/DashBourd.tsx
+++ initializereactproject/src/pages/DashBourd.tsx
@@ -1,74 +1,98 @@
-import React from 'react'
+import React from "react";
+import Card from "../components/Card";
 
 export default function DashBourd() {
-    const features = [
-        { name: "Origin", description: "Designed by Good Goods, Inc." },
-        {
-          name: "Material",
-          description:
-            "Solid walnut base with rare earth magnets and powder coated steel card cover",
-        },
-        { name: "Dimensions", description: '6.25" x 3.55" x 1.15"' },
-        { name: "Finish", description: "Hand sanded and finished with natural oil" },
-        { name: "Includes", description: "Wood card tray and 3 refill packs" },
-        {
-          name: "Considerations",
-          description:
-            "Made from natural materials. Grain and color vary with each item.",
-        },
-      ];
-      
-  return (
-     <div className="bg-white">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
-          <div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Technical Specifications
-            </h2>
-            <p className="mt-4 text-gray-500">
-              The walnut wood card tray is precision milled to perfectly fit a
-              stack of Focus cards. The powder coated steel divider separates
-              active cards from new ones, or can be used to archive important
-              task lists.
-            </p>
+  const features = [
+    { name: "Origin", description: "Designed by Good Goods, Inc." },
+    {
+      name: "Material",
+      description:
+        "Solid walnut base with rare earth magnets and powder coated steel card cover",
+    },
+    { name: "Dimensions", description: '6.25" x 3.55" x 1.15"' },
+    {
+      name: "Finish",
+      description: "Hand sanded and finished with natural oil",
+    },
+    { name: "Includes", description: "Wood card tray and 3 refill packs" },
+    {
+      name: "Considerations",
+      description:
+        "Made from natural materials. Grain and color vary with each item.",
+    },
+  ];
 
-            <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-              {features.map((feature) => (
-                <div
-                  key={feature.name}
-                  className="border-t border-gray-200 pt-4"
-                >
-                  <dt className="font-medium text-gray-900">{feature.name}</dt>
-                  <dd className="mt-2 text-sm text-gray-500">
-                    {feature.description}
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-          <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
-            <img
-              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
-              alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
-              className="rounded-lg bg-gray-100"
-            />
-            <img
-              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
-              alt="Top down view of walnut card tray with embedded magnets and card groove."
-              className="rounded-lg bg-gray-100"
-            />
-            <img
-              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
-              alt="Side of walnut card tray with card groove and recessed card area."
-              className="rounded-lg bg-gray-100"
-            />
-            <img
-              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
-              alt="Walnut card tray filled with cards and card angled in dedicated groove."
-              className="rounded-lg bg-gray-100"
-            />
-          </div>
+  const producs = [
+    {
+      name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      image:
+        "https://tripplem.lk/image/cache/catalog/tripplem/products/Electronic-Accessories/Wearable/Smart-Watches/Common-Brand/W26-Plus-Smart-Watch/W26-Plus-Smartwatch-1080x1140.jpg",
+      rating: 5,
+      price: 599,
+    },
+    {
+      name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      image:
+        "https://tripplem.lk/image/cache/catalog/tripplem/products/Electronic-Accessories/Wearable/Smart-Watches/Common-Brand/W26-Plus-Smart-Watch/W26-Plus-Smartwatch-1080x1140.jpg",
+      rating: 5,
+      price: 599,
+    },
+    {
+      name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      image:
+        "https://tripplem.lk/image/cache/catalog/tripplem/products/Electronic-Accessories/Wearable/Smart-Watches/Common-Brand/W26-Plus-Smart-Watch/W26-Plus-Smartwatch-1080x1140.jpg",
+      rating: 5,
+      price: 599,
+    },
+    {
+      name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      image:
+        "https://tripplem.lk/image/cache/catalog/tripplem/products/Electronic-Accessories/Wearable/Smart-Watches/Common-Brand/W26-Plus-Smart-Watch/W26-Plus-Smartwatch-1080x1140.jpg",
+      rating: 5,
+      price: 599,
+    },
+    {
+      name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      image:
+        "https://tripplem.lk/image/cache/catalog/tripplem/products/Electronic-Accessories/Wearable/Smart-Watches/Common-Brand/W26-Plus-Smart-Watch/W26-Plus-Smartwatch-1080x1140.jpg",
+      rating: 5,
+      price: 599,
+    },
+    {
+      name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      image:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ML763_VW_34FR+watch-case-45-stainless-gold-s9_VW_34FR+watch-face-45-stainless-gold-s9_VW_34FR?wid=750&hei=712&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1694507905569",
+      rating: 5,
+      price: 599,
+    },
+    {
+      name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      image:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ML763_VW_34FR+watch-case-45-stainless-gold-s9_VW_34FR+watch-face-45-stainless-gold-s9_VW_34FR?wid=750&hei=712&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1694507905569",
+      rating: 5,
+      price: 599,
+    },
+    {
+      name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      image:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ML763_VW_34FR+watch-case-45-stainless-gold-s9_VW_34FR+watch-face-45-stainless-gold-s9_VW_34FR?wid=750&hei=712&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1694507905569",
+      rating: 5,
+      price: 599,
+    }
+  ];
+
+  return (
+    <div className="bg-white m-auto">
+      <section className="flex flex-col">
+        <h1 className="text-4xl font-semibold p-4 text-gray-800">Welcome !</h1>
+      </section>
+      <section className="flex">
+        <div className="flex gap-5 flex-wrap justify-center mt-4 items-center mx-auto">
+          {producs.map((product, index) => (
+            <Card key={index} product={product} />
+          ))}
         </div>
-      </div>
-  )
+      </section>
+    </div>
+  );
 }
